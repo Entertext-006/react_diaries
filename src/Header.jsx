7a5a4d16d4088b0cfc99316e0cb6f9db20ad3603
Entertext-006 <dedeepya944@gmail.com>
2025-12-31 23:08:36 +0530

    Initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+function Header(){
+    return (
+        <header>
+            <h1>Welcome to My React App</h1>
+            <ul>
+                <li href="#">Home</li>
+                <li href="#">About</li>
+                <li href="#">Contact</li>
+            </ul>
+        </header>
+    );
+}
+export default Header;
